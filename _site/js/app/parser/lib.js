24e2cc20db82d174f18540af17599b67ea2b666c
molee1905 <mr.lee1905@gmail.com>
2015-12-21 17:19:12 +0800

    add .gitignore

--- _site/js/app/parser/lib.js
+++
@@ -1,8 +0,0 @@
-// requires
-var espree = require('espree');
-
-exports.parse = function(config) {
-	var code = editor.value;
-	var ast = espree.parse(code, config); // throws...
-	return JSON.stringify(ast, null, '  ');
-};
